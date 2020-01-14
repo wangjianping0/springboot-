dd169bce0919f415c02d95ce97e46844f9e6c8ed
cctvckl <914000408@qq.com>
2020-01-14 22:49:01 +0800

    namespace handler解析context:property-override元素的demo

---
+++ all-demo-in-spring-learning/spring-xml-demo/src/main/java/org/springframework/contextnamespace/Person.java
@@ -0,0 +1,11 @@
+package org.springframework.contextnamespace;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private int age;
+    private String location;
+
+}
