fdb6c5cb02ba4515a53600e55e89d3a38675266d
cctvckl <914000408@qq.com>
2020-02-05 23:39:16 +0800

    load-time-weave的demo

--- all-demo-in-spring-learning/spring-load-time-weave-demo/src/main/java/foo/Main.java
+++ all-demo-in-spring-learning/spring-load-time-weave-demo/src/main/java/foo/Main.java
@@ -8,8 +8,12 @@ public final class Main {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext(
                 "context-namespace-test-load-time-weave.xml");
-        StubEntitlementCalculationService entitlementCalculationService
-                = ctx.getBean(StubEntitlementCalculationService.class);
+//        StubEntitlementCalculationService entitlementCalculationService
+//                = ctx.getBean(StubEntitlementCalculationService.class);
+        /**
+         * 注意，这里直接new的
+         */
+        StubEntitlementCalculationService entitlementCalculationService = new StubEntitlementCalculationService();
 
         // the profiling aspect is 'woven' around this method execution
         entitlementCalculationService.calculateEntitlement();
